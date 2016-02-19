26479232ede24ce8d97fff1feee9dddd6f97f29b
Ayush-G <totallynotayush@outlook.com>
2016-02-19 14:11:51 -0500

    19/02/16/11:50

--- Hangman/Form1.Designer.vb
+++ Hangman/Form1.Designer.vb
@@ -22,16 +22,78 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.btnKeyboard = New System.Windows.Forms.Button()
+        Me.btnHKeyboard = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Word = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'btnKeyboard
+        '
+        Me.btnKeyboard.Location = New System.Drawing.Point(448, 12)
+        Me.btnKeyboard.Name = "btnKeyboard"
+        Me.btnKeyboard.Size = New System.Drawing.Size(75, 43)
+        Me.btnKeyboard.TabIndex = 0
+        Me.btnKeyboard.Text = "Show Keyboard"
+        Me.btnKeyboard.UseVisualStyleBackColor = True
+        Me.btnKeyboard.Visible = False
+        '
+        'btnHKeyboard
+        '
+        Me.btnHKeyboard.Location = New System.Drawing.Point(448, 12)
+        Me.btnHKeyboard.Name = "btnHKeyboard"
+        Me.btnHKeyboard.Size = New System.Drawing.Size(75, 43)
+        Me.btnHKeyboard.TabIndex = 1
+        Me.btnHKeyboard.Text = "Hide Keyboard"
+        Me.btnHKeyboard.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Failed Guesses"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(106, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 3
+        '
+        'Word
+        '
+        Me.Word.AutoSize = True
+        Me.Word.Location = New System.Drawing.Point(105, 259)
+        Me.Word.Name = "Word"
+        Me.Word.Size = New System.Drawing.Size(39, 13)
+        Me.Word.TabIndex = 4
+        Me.Word.Text = "Label3"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(588, 542)
+        Me.ClientSize = New System.Drawing.Size(944, 542)
+        Me.Controls.Add(Me.Word)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnHKeyboard)
+        Me.Controls.Add(Me.btnKeyboard)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents btnKeyboard As Button
+    Friend WithEvents btnHKeyboard As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Word As Label
 End Class
--- Hangman/Form1.vb
+++ Hangman/Form1.vb
@@ -1,11 +1,25 @@
 ﻿Public Class Form1
     Public letters(25) As Button
+    Dim accept As Integer
+    Dim space As Button
     Dim buttonpoint As Point
     Dim qwerty As String = "QWERTYUIOPASDFGHJKLZXCVBNM"
     Dim myfont As Font = New Font(FontFamily.GenericSansSerif, 14.0F, FontStyle.Bold)
+    Dim wordnum As Integer
+    Dim guessletter As String
+    Dim guesses As String
+    Dim strWord As String
+    Dim correct As Boolean
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        buttonpoint.X = 35
-        buttonpoint.Y = 35
+        Randomize()
+        accept = MsgBox("Accept?", 4, "Accept")
+        If accept = 6 Then
+            Me.Show()
+        Else
+            End
+        End If
+        buttonpoint.X = 535
+        buttonpoint.Y = 65
         'Define buttons in button array
         For x = 0 To 25
             letters(x) = New Button
@@ -19,21 +33,80 @@
             letters(x).FlatStyle = FlatStyle.Flat
             letters(x).Font = myfont
             If x = 9 Then
-                buttonpoint.Y = 75
-                buttonpoint.X = 55
+                buttonpoint.Y = 105
+                buttonpoint.X = 555
             End If
             If x = 18 Then
-                buttonpoint.Y = 115
-                buttonpoint.X = 75
+                buttonpoint.Y = 145
+                buttonpoint.X = 575
             End If
             AddHandler letters(x).Click, AddressOf Clicked
             Me.Controls.Add(letters(x))
         Next
+        buttonpoint.X = 655
+        buttonpoint.Y = 185
+        space = New Button
+        space.Width = 195
+        space.Height = 35
+        space.Location = buttonpoint
+        space.BackColor = Color.DodgerBlue
+        space.ForeColor = Color.White
+        space.FlatStyle = FlatStyle.Flat
+        space.Font = myfont
+        space.Text = "Space"
+        AddHandler space.Click, AddressOf Clicked
+        Me.Controls.Add(space)
+
+
+        'Word Gen
+        wordnum = 5 * Rnd()
+        If wordnum = 0 Then
+            strWord = "Hello"
+        ElseIf wordnum = 1
+            strWord = "Hi"
+        ElseIf wordnum = 2
+            strWord = "Yes"
+        ElseIf wordnum = 3
+            strWord = "No"
+        ElseIf wordnum = 4
+            strWord = "Bye"
+        Else strWord = "Goodbye"
+        End If
+        Word.Text = strWord
 
     End Sub
 
     Private Sub Clicked(sender As System.Object, e As System.EventArgs)
-
+        guesses = guesses & sender.text
+        guessletter = sender.text
+        For x = 1 To Len(strWord)
+            If UCase(Mid(strWord, x, 1)) Like sender.text Then
+                MsgBox("Yes")
+            End If
+        Next
+        sender.visible = False
+        For x = 1 To Len(strWord)
+            For y = 1 To Len(guesses)
+                If Mid(strWord, x, 1) Like Mid(guesses, y, 1) Then
+                    correct = True
+                Else correct = False
+                End If
+            Next
+        Next
+        If correct = True Then
+            MsgBox("Congrats, you win!")
+        End If
     End Sub
 
+    Private Sub btnKeyboard_Click(sender As Object, e As EventArgs) Handles btnKeyboard.Click
+        Me.Width = 960
+        sender.hide
+        btnHKeyboard.Show()
+    End Sub
+
+    Private Sub btnHKeyboard_Click(sender As Object, e As EventArgs) Handles btnHKeyboard.Click
+        Me.Width = 544
+        sender.hide
+        btnKeyboard.Show()
+    End Sub
 End Class
